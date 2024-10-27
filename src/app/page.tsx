320a17cf26711c35153355db95467287a4063f3e
lcsilva76 <lsilva@fiap.com.br>
2024-10-27 18:19:44 -0300

    Terceiro Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-
-
 export default function Home() {
   return (
     <div className="bg-indigo-500 h-screen w-screen flex justify-center items-center">
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Sobre() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <h1 className='text-3xl font-bold'>Sobre o Projeto</h1>
+    </div>
+  )
+}
